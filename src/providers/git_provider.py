8728a3a31b11311add1c1f30c88b664f43cafa7a
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-01-30 14:19:33 +0100

    Update default branch in clone_git_repo method

--- src/providers/git_provider.py
+++ src/providers/git_provider.py
@@ -8,7 +8,7 @@ class gitProvider:
         logger = logging.getLogger(__name__)
         self.logger = logger
     
-    def clone_git_repo(self, url, destination, branch = 'master'):
+    def clone_git_repo(self, url, destination, branch = 'mvp2'):
         self.logger.info(f"Cloning branch {branch} from repo {url}")
         try:
             repo = git.Repo.clone_from(url, destination, branch=branch)
